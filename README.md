316a5cdfa83c48e3e806dc7293f2a6bfafae0f5e
BBQuercus <bastian@eichenbergers.ch>
2022-09-28 11:58:41 +0200

    Add initial set of partially functional widgets

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# koopa-viz
-
 [![License MIT](https://img.shields.io/pypi/l/koopa-viz.svg?color=green)](https://github.com/bbquercus/koopa-viz/raw/main/LICENSE)
 [![PyPI](https://img.shields.io/pypi/v/koopa-viz.svg?color=green)](https://pypi.org/project/koopa-viz)
 [![Python Version](https://img.shields.io/pypi/pyversions/koopa-viz.svg?color=green)](https://python.org)
@@ -7,19 +5,11 @@
 [![codecov](https://codecov.io/gh/bbquercus/koopa-viz/branch/main/graph/badge.svg)](https://codecov.io/gh/bbquercus/koopa-viz)
 [![napari hub](https://img.shields.io/endpoint?url=https://api.napari-hub.org/shields/koopa-viz)](https://napari-hub.org/plugins/koopa-viz)
 
+# koopa-viz
+
 Vizualization plugin for koopa image analysis
 
-----------------------------------
-
 This [napari] plugin was generated with [Cookiecutter] using [@napari]'s [cookiecutter-napari-plugin] template.
-
-<!--
-Don't miss the full getting started guide to set up your new package:
-https://github.com/napari/cookiecutter-napari-plugin#getting-started
-
-and review the napari docs for plugin developers:
-https://napari.org/stable/plugins/index.html
--->
 
 ## Installation
 
@@ -27,12 +17,9 @@ You can install `koopa-viz` via [pip]:
 
     pip install koopa-viz
 
-
-
 To install latest development version :
 
     pip install git+https://github.com/bbquercus/koopa-viz.git
-
 
 ## Contributing
 
@@ -52,16 +39,6 @@ If you encounter any problems, please [file an issue] along with a detailed desc
 [Cookiecutter]: https://github.com/audreyr/cookiecutter
 [@napari]: https://github.com/napari
 [MIT]: http://opensource.org/licenses/MIT
-[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
-[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
-[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
-[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
-[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
-[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
-
 [file an issue]: https://github.com/bbquercus/koopa-viz/issues
-
-[napari]: https://github.com/napari/napari
 [tox]: https://tox.readthedocs.io/en/latest/
 [pip]: https://pypi.org/project/pip/
-[PyPI]: https://pypi.org/
--- src/koopa_viz/_widget.py
+++ src/koopa_viz/_widget.py
@@ -1,46 +1,216 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
+import configparser
+import glob
+import os
 
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
+from qtpy.QtWidgets import (
+    QComboBox,
+    QFrame,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QFileDialog,
+    QVBoxLayout,
+    QWidget,
+    QProgressBar,
+)
+import pandas as pd
+import tifffile
 
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
 
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
-
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+class KoopaWidget(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
+
+        # General config
         self.viewer = napari_viewer
+        self.spots_cols = ["frame", "y", "x"]
+        self.track_cols = ["particle", "frame", "y", "x"]
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        # Build plugin layout
+        self.setLayout(QVBoxLayout())
+        self.setup_config_parser()
+        self.layout().addWidget(self.get_separator_line())
+        self.setup_file_dropdown()
+        self.layout().addWidget(self.get_separator_line())
+        self.setup_save_widget()
+        self.layout().addWidget(self.get_separator_line())
+        self.setup_file_navigation()
+        self.layout().addWidget(self.get_separator_line())
+        self.setup_progress_bar()
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+    def get_separator_line(self):
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        return line
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+    def setup_config_parser(self):
+        """Prepare widget for config reader."""
+        widget = QWidget()
+        widget.setLayout(QVBoxLayout())
+        widget.layout().addWidget(QLabel("Analysis Directory"))
 
+        btn_widget = QPushButton("Select")
+        btn_widget.clicked.connect(self.open_file_dialog)
+        widget.layout().addWidget(btn_widget)
+        self.layout().addWidget(widget)
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    def setup_file_dropdown(self):
+        widget = QWidget()
+        widget.setLayout(QVBoxLayout())
+        widget.layout().addWidget(QLabel("Current File"))
 
+        self.dropdown_widget = QComboBox()
+        widget.layout().addWidget(self.dropdown_widget)
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+        btn_widget = QPushButton("Load")
+        btn_widget.clicked.connect(self.load_file)
+        widget.layout().addWidget(btn_widget)
+
+        self.file_dropdown = widget
+        self.file_dropdown.setDisabled(True)
+        self.layout().addWidget(self.file_dropdown)
+
+    def setup_save_widget(self):
+        widget = QWidget()
+        widget.setLayout(QVBoxLayout())
+        widget.layout().addWidget(QLabel("Save Edits"))
+        btn_widget = QPushButton("Run")
+        btn_widget.clicked.connect(self.save_edits)
+        widget.layout().addWidget(btn_widget)
+
+        self.save_widget = widget
+        self.save_widget.setDisabled(True)
+        self.layout().addWidget(self.save_widget)
+
+    def setup_file_navigation(self):
+        widget = QWidget()
+        widget.setLayout(QHBoxLayout())
+        widget.layout().addWidget(QLabel("Navigate Files"))
+        prev_widget = QPushButton("Prev Image")
+        prev_widget.clicked.connect(lambda: self.change_file("prev"))
+        widget.layout().addWidget(prev_widget)
+        next_widget = QPushButton("Next Image")
+        next_widget.clicked.connect(lambda: self.change_file("next"))
+        widget.layout().addWidget(next_widget)
+
+        self.file_navigation = widget
+        self.file_navigation.setDisabled(True)
+        self.layout().addWidget(self.file_navigation)
+
+    def setup_progress_bar(self):
+        self.pbar = QProgressBar(self)
+        self.pbar.setValue(0)
+        self.layout().addWidget(self.pbar)
+
+    def open_file_dialog(self):
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.AnyFile)
+        if dialog.exec_():
+            self.analysis_path = dialog.selectedFiles()[0]
+            self.run_config_parser()
+            self.get_file_list()
+
+    def run_config_parser(self):
+        """Retrieve config file from analysis directory."""
+        config_file = os.path.join(
+            os.path.abspath(self.analysis_path), "koopa.cfg"
+        )
+        if not os.path.exists(config_file):
+            raise ValueError("Koopa config file does not exist!")
+
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
+    def load_file(self):
+        """Ope n all associated files and enable editing."""
+        self.name = self.dropdown_widget.currentText()
+
+        self.pbar.setValue(0)
+        self.load_image()
+        self.pbar.setValue(25)
+        self.load_segmentation_cells()
+        self.pbar.setValue(50)
+        if eval(self.config["SegmentationOther"]["enabled"]):
+            self.load_segmentation_other()
+        self.pbar.setValue(90)
+        self.load_segmentation_raw()
+        self.pbar.setValue(100)
+        if eval(self.config["SpotsColocalization"]["enabled"]):
+            self.load_colocalization()
+        self.save_widget.setDisabled(False)
+
+    def save_edits(self):
+        print("saving shit")
+
+    def change_file(self, option: str):
+        print(f"option is {option}")
+
+    def get_file_list(self):
+        files = sorted(
+            glob.glob(
+                os.path.join(self.analysis_path, "preprocessed", "*.tif")
+            )
+        )
+        files = sorted(
+            [os.path.basename(f).replace(".tif", "") for f in files]
+        )
+        self.file_dropdown.setDisabled(False)
+        self.file_navigation.setDisabled(False)
+        self.dropdown_widget.addItems(files)
+
+    def load_image(self):
+        fname = os.path.join(
+            self.analysis_path, "preprocessed", f"{self.name}.tif"
+        )
+        image = tifffile.imread(fname)
+        for idx, channel in enumerate(image):
+            self.pbar.setValue(25 / len(image) * idx)
+            self.viewer.add_image(
+                channel, name=f"Channel {idx}", blending="additive"
+            )
+
+    def load_segmentation_cells(self):
+        fname = os.path.join(
+            self.analysis_path, "segmentation_primary", f"{self.name}.tif"
+        )
+        segmap = tifffile.imread(fname).astype(int)
+        self.viewer.add_labels(
+            segmap, name="Segmentation Primary", blending="translucent"
+        )
+
+    def load_segmentation_other(self):
+        for channel in eval(self.config["SegmentationOther"]["channels"]):
+            fname = os.path.join(
+                self.analysis_path,
+                f"segmentation_{channel}",
+                f"{self.name}.tif",
+            )
+            segmap = tifffile.imread(fname).astype(int)
+            self.viewer.add_labels(
+                segmap, name=f"Segmentation C{channel}", blending="translucent"
+            )
+
+    def load_segmentation_raw(self):
+        do_timeseries = eval(self.config["General"]["do_TimeSeries"])
+        do_3D = eval(self.config["General"]["do_3D"])
+
+        for channel in eval(self.config["SpotsDetection"]["channels"]):
+            folder = (
+                f"detection_final_c{channel}"
+                if do_3D or do_timeseries
+                else f"detection_raw_c{channel}"
+            )
+            fname = os.path.join(
+                self.analysis_path, folder, f"{self.name}.parq"
+            )
+            df = pd.read_parquet(fname)
+            if do_timeseries:
+                self.viewer.add_tracks(
+                    df[self.track_cols], name=f"Track C{channel}"
+                )
+            else:
+                self.viewer.add_points(
+                    df[self.spots_cols], name=f"Detection C{channel}"
+                )
+
